e20dc986d9be2bebcba3b10e61e597ec61a4a343
Michael Cowie <mco116@uclive.ac.nz>
2017-01-17 04:34:37 +1300

    Update swapIfGreater.c

--- swapIfGreater.c
+++ swapIfGreater.c
@@ -1,4 +1,14 @@
 /*Write a function swapIfGreater that exchanges the values of two int variables if the first one is greater than the second*/
+/*
+Example:
+int first = 20;
+int second = 10;
+swapIfGreater(&first, &second);
+// Values have been swapped
+printf("%d %d\n", first, second);
+Result:
+10 20
+*/
 
 void swapIfGreater(int* x, int* y)
 {
