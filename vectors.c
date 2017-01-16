efe5fc144c631310dc43f409c67ba229945e0513
Michael Cowie <mco116@uclive.ac.nz>
2017-01-17 04:25:01 +1300

    Update vectors.c

--- vectors.c
+++ vectors.c
@@ -20,3 +20,13 @@ Vector2d vectorSum(Vector2d v1, Vector2d v2)
 	aVector2.y = v1.y + v2.y;
 	return aVector2;
 }
+
+/*
+Test:
+Vector2d v1 = vector(100, -97);
+Vector2d v2 = vector(11, 1);
+Vector2d v3 = vectorSum(v1, v2);
+printf("(%d, %d) + (%d, %d) = (%d, %d)\n",
+        v1.x, v1.y, v2.x, v2.y, v3.x, v3.y);
+	
+/*
