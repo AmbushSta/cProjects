3a4224b845bc2c6d829525d256f4963abcda910c
Michael Cowie <mco116@uclive.ac.nz>
2017-01-17 04:31:35 +1300

    Update and rename primesBetweenXandY to primesBetweenXandY.c

--- primesBetweenXandY.c
+++ primesBetweenXandY.c
@@ -7,25 +7,25 @@ The program should then print all the prime numbers between n1 and n2 inclusive.
 
 
 
-int main()
-{	
+#include <stdio.h> 
 
-	int n1 = 0;
+int main(void)
+
+
+{
+	int n1 =  0;
 	int n2 = 0;
-	scanf("%d %d", &n1, &n2);
-	if(n1 == 2){
-		printf("2\n");
-	}
+	scanf("%d %d", &n1,&n2);
 	for(int i = n1; i <= n2; i++){
 		for(int x = 2; x <= i; x++){
-			if(i % x == 0){
-				break;
-			} else if(x == i -1){;
+			if(i == x){
 				printf("%d\n",i);
 			}
+			else if(i % x == 0){
+				break;
+			}
 		}
+		
 	}
-
-
 	return 0;
 }
