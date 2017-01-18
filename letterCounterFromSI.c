23c8f6943b5ee3828b67022ab11dd258f9ebb0ca
Michael Cowie <mco116@uclive.ac.nz>
2017-01-18 18:16:30 +1300

    Update letterCounterFromSI.c

--- letterCounterFromSI.c
+++ letterCounterFromSI.c
@@ -1,8 +1,5 @@
-/*Write a C program that reads its standard input until EOF occurs and prints out a table of how many
-times each of the 26 letters in the alphabet occurred. Ignore all non-alphabetic characters. 
-The program should be case-insensitive, i.e., each letter count 
-includes both the lowercase and the uppercase letters.*/
-/*
+/*Reads from standard input until EOF and displays the count of each character
+
 Example:
 Input:
 aBbcCcDDDDeeeeefFfFfFG
@@ -14,7 +11,7 @@ D: 4
 E: 5
 F: 6
 G: 1
-Also prints letters which include X: 0, but are not shown in this docstring
+Also prints letters which include 0, but are not shown in this docstring
 */
 
 #include <stdio.h>
@@ -23,17 +20,16 @@ Also prints letters which include X: 0, but are not shown in this docstring
 int main(void)
 
 {
-	int array[26] = {0};
-	char c = getchar();
-	while(c != EOF){
-		if(isalpha(c)){
-			c = toupper(c);
-			array[c-65] = array[c-65] + 1;
-		}
-		c = getchar();
+    int array[26] = {0};
+    char c = getchar();
+    while(c != EOF){
+	if(isalpha(c)){
+		c = toupper(c);
+		array[c-65] = array[c-65] + 1;
+	        }
+	c = getchar();
 	}
-	
 	for(int x = 0; x <= 25; x++){
-		printf("%c: %d\n", x + 65, array[x]);
-	}
+	    printf("%c: %d\n", x + 65, array[x]);
+    }
 }
