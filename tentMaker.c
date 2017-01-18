4c1e34e60ddb39ef4588da43f9fd04a29bd1f1a7
Michael Cowie <mco116@uclive.ac.nz>
2017-01-18 18:38:51 +1300

    Update tentMaker.c

--- tentMaker.c
+++ tentMaker.c
@@ -1,5 +1,5 @@
-/*Write a function with signature void printTent(int n) that displays an open "tent" shape of stars of size n */
-/* 
+/*Displays an open "tent" shape of stars of size n 
+
 Example:
 printTent(5)
     *
@@ -10,24 +10,19 @@ printTent(5)
 */
 
 void printTent(int n)
-{	
-	
-	for(int row = 1; row <= n; row++){
-		for(int space = 1; space <= n - row; space ++){
-			printf(" ");
-		}
-		
-		printf("*");
-		
-		if(row != 1){
-			for(int i = 1; i <= (row * 2) - 3; i++){
-				printf(" ");
-			}
-			printf("*\n");
-		}else{
-			printf("\n");
-		}
+{
+    for(int row = 1; row <= n; row++){
+	for(int space = 1; space <= n - row; space ++){
+	    printf(" ");
 	}
-	
-	
+	printf("*");
+	if(row != 1){
+		for(int i = 1; i <= (row * 2) - 3; i++){
+	    	    printf(" ");
+		}
+	printf("*\n");
+	}else{
+		printf("\n");
+	}
+    }
 }
