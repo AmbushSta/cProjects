723d6678207021f4c1dd3ad735bd6fb590afec09
Michael Cowie <mco116@uclive.ac.nz>
2017-01-18 18:12:21 +1300

    Update charCounter.c

--- charCounter.c
+++ charCounter.c
@@ -1,5 +1,5 @@
-/*Write a function with signature int countChars(const char* s, const char* charsOfInterest) 
-that returns a count of the number of characters in the string s that also appear in the string charsOfInterest.*/
+/*Returns a count of the number of characters in the string s 
+that also appear in the string charsOfInterest.*/
 
 /*
 Example:
@@ -9,14 +9,14 @@ Result:
 */
 int countChars(const char* s, const char* charsOfInterest)
 {
-	int counter = 0;
-	for(int i = 0; i < strlen(s);i++){
-		for(int n = 0; n < strlen(charsOfInterest);n++){
-			if(s[i] == charsOfInterest[n]){
-				counter += 1;
-			}
+    int counter = 0;
+    for(int i = 0; i < strlen(s);i++){
+	for(int n = 0; n < strlen(charsOfInterest);n++){
+		if(s[i] == charsOfInterest[n]){
+		counter += 1;
 		}
 	}
-	return counter;
+    }
+    return counter;
 	
 }
