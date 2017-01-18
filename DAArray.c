dc74087a6f08c1b9e8c0dedb79357340467a5035
Michael Cowie <mco116@uclive.ac.nz>
2017-01-18 18:08:29 +1300

    Update DAArray.c

--- DAArray.c
+++ DAArray.c
@@ -1,6 +1,6 @@
-/*Write a function with signature int* ramp(int n) that returns a pointer to a n-element dynamically-allocated array
-of ints containing the values 1, 2, 3, ... n.  You may assume n is at least 1.*/
-/*
+/*Returns a pointer to a n-element dynamically-allocated array
+of ints containing the values 1, 2, 3, ... n.
+
 Example:
 int* data = ramp(5);
 for (int i = 0; i < 5; i++) {
@@ -8,9 +8,7 @@ for (int i = 0; i < 5; i++) {
 }
 free(data);
 Result:
-1 2 3 4 5
-*/
-
+1 2 3 4 5*/
 
 int* ramp(int n)
 {
