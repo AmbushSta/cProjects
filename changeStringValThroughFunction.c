2ad664b25abcc76e730293bb79b4658574f2be67
Michael Cowie <mco116@uclive.ac.nz>
2018-06-28 00:07:59 +1200

    Update changeStringValThroughFunction.c

--- changeStringValThroughFunction.c
+++ changeStringValThroughFunction.c
@@ -5,8 +5,8 @@
 #include <stdio.h>
 
 void changeStringVal(char* input){
-    input++;
-    *(input + 1) += 5;
+    input++; // Increase pointer by 1 type of bytes (i.e int = 4 memory addresses, char = 1 memory address)
+    *(input + 1) += 5; // Derefernce pointer and increase value by 5
     printf("%s", input); //28
 }
 
