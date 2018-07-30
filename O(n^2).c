f23971c966452f99d7c7c02255a9fa16191615ee
Michael Cowie <mco116@uclive.ac.nz>
2018-07-30 23:29:54 +1200

    Update O(n^2).c

--- O(n^2).c
+++ O(n^2).c
@@ -9,21 +9,21 @@
 #include <string.h>
 
 
-void swap_indexes(int* list, int i_one, int i_two){
+void swap_indexes(int *list, int i_one, int i_two) {
     int temp = list[i_one];
     list[i_one] = list[i_two];
     list[i_two] = temp;
 }
 
-int* sort(int* unsorted, int len){
-     int* sorted = malloc(len * sizeof(int));
+int* sort(int *unsorted, int len) {
+     int *sorted = malloc(len * sizeof(int));
      memcpy(sorted, unsorted, len * sizeof(int));
      
-    for(int place = 0; place < len; place++){
+    for (int place = 0; place < len; place++) {
         int smallest_index = place;
         int smallest_num = sorted[place];
         
-        for(int i = place; i < len; i ++){
+        for(int i = place; i < len; i++) {
             if(sorted[i] < smallest_num){
                 smallest_index = i;
                 smallest_num = sorted[i];
@@ -35,11 +35,10 @@ int* sort(int* unsorted, int len){
     return sorted;
 }
 
-int main()
-{
+int main() {
     int len = 5;
-    int* unsorted = malloc(len * sizeof(int));
-    for(int i = 0; i < len; i ++){
+    int *unsorted = malloc(len * sizeof(int));
+    for (int i = 0; i < len; i++) {
         unsorted[i] = 4 - i;
     }
     
