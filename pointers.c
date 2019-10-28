8c459bcfd3fd70cd9b97645664bd8a1fbb1e9c63
Michael Cowie <mco116@uclive.ac.nz>
2019-10-28 17:16:14 +1300

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -8,7 +8,13 @@ void pointerFunc(int* iptr){
     /* Print the value pointed to by iptr */
     printf("value %d\n", *iptr);
     
-    /* Print the address pointed to by iptr */
+    /* 
+       Print the address pointed to by iptr.
+       By previously specifying that it is an int
+       pointer, we now know the value will be at
+       this address + 32 bits of range.
+    */
+    
     printf("located at memory address %p\n", iptr);
     
     /* Print the address of iptr */
